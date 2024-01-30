6465e7f8639aa0454a62224d2221e82cf1a69605
ilyavorobyov <ilyavorobyov@outlook.com>
2024-01-30 14:14:32 +0300

    3

---
+++ Assets/Scripts/SDKInitializer.cs
@@ -0,0 +1,30 @@
+using Agava.YandexGames;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public sealed class SDKInitializer : MonoBehaviour
+{
+    private const string GameSceneName = "GameScene";
+
+    private void Awake()
+    {
+        YandexGamesSdk.CallbackLogging = true;
+    }
+
+    private IEnumerator Start()
+    {
+        yield return YandexGamesSdk.Initialize(OnInitialized);
+    }
+
+    private void OnInitialized()
+    {
+        OnCallGameReadyButtonClick();
+        SceneManager.LoadScene(GameSceneName);
+    }
+
+    private void OnCallGameReadyButtonClick()
+    {
+        YandexGamesSdk.GameReady();
+    }
+}
